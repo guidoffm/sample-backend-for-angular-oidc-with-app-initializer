566e3c6f77f7c09f108147e3dc0fa5f5fcd63356
GuidoGuMueller <guido.gu.mueller-extern@deutschebahn.com>
2021-04-24 10:25:29 +0200

    Updated index.js README.md

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ The public key used here as an environment variable is taken
 from the OIDC issuer.
 
 ```bash
-PUBLIC_KEY='-----BEGIN PUBLIC KEY-----
+PUBLIC_KEY='
 -----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuOFYQG+bb3OlDoVMLoEA
 EmW3xhBVrNU8qaPQGPUT7GBZPMEdg/MUAD5PW6yPr4rFbaDYhHVBzrT/kiM0dv5t
@@ -15,5 +15,6 @@ eVZv2FILyJ4SldoPdzbi43PvgvfAzlA8ZV7FN/idx2zxjNAgdP26bj+Q9hDo0u2B
 43OwdRkVWnEvdR4htCFUch/BR3CS98OOIqk0Udtm7LlLJ/p4PiT42Ix7BKOkqUDo
 srqB3pYfyxW7K/leFWV38NNRwNxR1pHks+te33dHiS4YTFDYrV45NdlA+Oj5XkWn
 rQIDAQAB
------END PUBLIC KEY-----' node index
+-----END PUBLIC KEY-----
+' node index
 ```
--- index.js
+++ index.js
@@ -7,29 +7,49 @@ const port = 3000;
 const app = express();
 
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
+  let token;
+  if (req.query.access_token) {
+    token = req.query.access_token;
+  } else {
+    const authHeader = req.headers['authorization']
+    token = authHeader && authHeader.split(' ')[1]
+  }
 
   if (token == null) {
     return res.sendStatus(401);
   }
 
-  verify(token, process.env.PUBLIC_KEY, (err, user) => {
+  verify(token, process.env.PUBLIC_KEY, (err, tokenData) => {
 
     if (err) {
       console.log(err);
       return res.sendStatus(403);
     }
 
-    req.user = user;
+    // token expired
+    if (Date.now() > tokenData.exp * 1000) {
+      return res.sendStatus(403);
+    }
+
+    // token note before (nbf)
+    if (tokenData.nbf && Date.now() < tokenData.nbf * 1000) {
+      return res.sendStatus(403);
+    }
+
+    // Optional: check group membership
+    // if (requiredGroup && tokenData[groupsClaimName].split(groupsSeparator).filter(x => x === requiredGroup).length != 1) {
+    //   return res.sendStatus(403);
+    // }
+
+    req.token = tokenData;
 
     next();
   });
 };
 
 app.get('/api/test', authenticateToken, (req, res) => {
-  console.log(req.user);
-  res.send(req.user);
+  console.log(req.token);
+  res.send(req.token);
 });
 
 app.listen(port, () => {
